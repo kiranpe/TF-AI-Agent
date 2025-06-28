514ad08cd8e9b9366cc20edd4624ac975dc5ffe6
Sahithi Nallamotu <kiranwls2011@gmail.com>
2025-06-28 02:12:37 -0700

    Updated yml file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 
-variable "project_id" {}
+variable "project_id" { default = "mlops-448320" }
 variable "region" { default = "us-east1" }
 variable "domain" { default = "cloud-devops.in" }
 variable "domain_with_dot" { default = "cloud-devops.in." }
